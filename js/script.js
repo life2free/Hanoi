082b146633def0f9fd4fca2a926cb4010a5bc713
shimin <thunderous_0518@hotmail.com>
2020-03-24 16:08:44 -0400

    finish the basic page layout

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Tower of Hanoi
+ * Author: Shimin Rao
+ * Date: 3/24/2020
+ * Version: 1.0.0
+ */
+
+/* 
+
+*/
+// const stacks = $(".stack");
+
+// function test() {
+//   alert($(this).attr("id") + "Handler for .click() called.");
+// }
+// $(document).ready(function() {
+//   $(".stack").click(test);
+// });
+
+// stacks.each(element => {
+//   console.log(stacks[element]);
+// });
